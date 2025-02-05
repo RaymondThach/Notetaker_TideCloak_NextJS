11eef2978abd9e6bfe4c1997b630175877958bc1
RaymondThach <raymondnthach@gmail.com>
2025-02-05 12:59:47 +1000

    added initial styling to modal, save button, cancel button, close button, title text field, note text field, and headers

--- pages/Modal.js
+++ pages/Modal.js
@@ -3,7 +3,21 @@ import styles from "./modal.module.css";
 export default function Modal() {
     return (
         <div className={styles.modal}>
-            <p>Hello</p>
+            <div className={styles.container}>
+                <div className={styles.topMenu}>
+                    <button>X</button>
+                </div>
+                <h className={styles.header}>Note</h>
+                <p className={styles.titleHeader}>Title:</p>
+                <input className={styles.titleInput} name="titleInput" />
+                <p className={styles.noteHeader}>Notes:</p>
+                <input className={styles.noteInput} name="noteInput" />
+                <div className={styles.bottomMenu}>
+                    <button>Save</button>
+                    <button>Cancel</button>
+                </div>
+            </div>
+            
         </div>
     );
 }
